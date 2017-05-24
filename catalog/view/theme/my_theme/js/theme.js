17d798cc14e2a7c8f6a1b9c588188fd09b9c29c5
Muslim Maliukov <Muslim.krum.85@gmail.com>
2017-05-24 19:58:29 +0300

    home page category & product dev

--- catalog/view/theme/my_theme/js/theme.js
+++ catalog/view/theme/my_theme/js/theme.js
@@ -8,4 +8,11 @@ $(document).ready(function(){
       $(this).addClass("active");
     }
   });
+  $('.product-layout > .product-thumb').each(function (current) {
+    current += 1;
+    $(this).find('.button-group').append('<a class="button-toclick" href="#current-item-'+ current +'">Купить в один клик</a>');
+    $(this).parent().attr({
+      'class': 'product-layout col-lg-4 col-md-4 col-sm-6 col-xs-12'
+    });
+  })
 });
