1ffcd42e0622a92f9b06ad70d63430814ec19125
Muslim Maliukov <Muslim.krum.85@gmail.com>
2017-05-25 18:39:57 +0300

    home page category & product dev: sale script, hover effect,button style, text style

--- catalog/view/theme/my_theme/js/theme.js
+++ catalog/view/theme/my_theme/js/theme.js
@@ -1,6 +1,7 @@
 /**
  * Created by user on 5/21/2017.
  */
+//добавление активного класса к пунктам меню
 $(document).ready(function(){
   $('.col-sm-9 > nav ul li a').each(function(){
     if(this.href == window.location.href) {
@@ -8,11 +9,18 @@ $(document).ready(function(){
       $(this).addClass("active");
     }
   });
+  //увеличение ширины стандартных айтемов товаров (col-lg-3 -> col-lg-4)
   $('.product-layout > .product-thumb').each(function (current) {
     current += 1;
     $(this).find('.button-group').append('<a class="button-toclick" href="#current-item-'+ current +'">Купить в один клик</a>');
     $(this).parent().attr({
       'class': 'product-layout col-lg-4 col-md-4 col-sm-6 col-xs-12'
     });
+  })
+  //добавление элемента "акция" к айтему товара
+  $('.product-layout > .product-thumb').each(function () {
+    if($(this).find('.price-old').length > 0) {
+      $(this).find('.image').prepend('<div class="stock">SALE</div>');
+    } else return;
   })
 });
