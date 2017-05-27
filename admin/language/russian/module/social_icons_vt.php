93a00a285106ad909ca4e0ccafe1401573669b48
Muslim Maliukov <Muslim.krum.85@gmail.com>
2017-05-27 11:28:37 +0300

    left sidebar dev

---
+++ admin/language/russian/module/social_icons_vt.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']     = 'Social icons VT';
+
+// Text
+$_['text_module']       = 'Модули';
+$_['text_success']      = 'Настройки модуля обновлены!';
+$_['text_edit']         = 'Редактирование модуля';
+
+$_['text_url']          = 'Вставьте URL';
+
+$_['text_facebook_vt']  = 'Facebook';
+$_['text_google_vt']    = 'Google+';
+$_['text_youtube_vt']   = 'Youtube';
+$_['text_instagram_vt'] = 'Instagram';
+$_['text_twitter_vt']   = 'Twitter';
+$_['text_vk_vt']        = 'Вконтаке';
+
+// Entry
+$_['entry_status']      = 'Статус';
+
+// Error
+$_['error_permission']  = 'У Вас нет прав для управления этим модулем!';
---
+++ catalog/language/english/module/social_icons_vt .php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title_vt'] = 'Будте с нами';
+
+// Text
+$_['text_facebook_vt']  = 'Facebook';
+$_['text_google_vt']  = 'Google+';
+$_['text_youtube_vt']  = 'Youtube';
+$_['text_instagram_vt']  = 'Instagram';
+$_['text_twitter_vt']  = 'Twitter';
+$_['text_vk_vt']  = 'Вконтаке';
+
+
+
+
